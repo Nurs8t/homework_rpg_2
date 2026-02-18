37f46b39513e42ab86ed080fa1f6fbbcba673d81
Nurs8t <nurseiit.atystanbek@narxoz.kz>
2026-02-18 18:58:45 +0500

    ice abstract factory

---
+++ src/com/narxoz/rpg/factory/IceComponentFactory.java
@@ -0,0 +1,27 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.*;
+import com.narxoz.rpg.combat.ice.FrostBreath;
+import com.narxoz.rpg.combat.ice.IceShield;
+import com.narxoz.rpg.loot.*;
+
+import java.util.List;
+
+public class IceComponentFactory implements EnemyComponentFactory {
+
+    @Override
+    public List<Ability> createAbilities() {
+        return List.of(new FrostBreath(), new IceShield()
+        );
+    }
+
+    @Override
+    public LootTable createLootTable() {
+        return new IceLootTable();
+    }
+
+    @Override
+    public String createAIBehavior() {
+        return "Defensive";
+    }
+}
