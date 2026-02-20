54257e5abb6fde799a77658e49359730e3b6062c
Nurs8t <nurseiit.atystanbek@narxoz.kz>
2026-02-20 23:18:56 +0500

    enemybuil

---
+++ src/com/narxoz/rpg/builder/EnemyBuilder.java
@@ -0,0 +1,21 @@
+package com.narxoz.rpg.builder;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.enemy.Enemy;
+import com.narxoz.rpg.loot.LootTable;
+
+import java.util.List;
+
+public interface EnemyBuilder {
+
+    EnemyBuilder setName(String name);
+    EnemyBuilder setHealth(int health);
+    EnemyBuilder setDamage(int damage);
+    EnemyBuilder setDefense(int defense);
+    EnemyBuilder setSpeed(int speed);
+
+    EnemyBuilder setAbilities(List<Ability> abilities);
+    EnemyBuilder setLootTable(LootTable lootTable);
+
+    Enemy build();
+}
